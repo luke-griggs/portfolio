37b6959f9664c12d51f67246d6c6802aacb217e8
Luke <lukegriggs100@gmail.com>
2024-07-14 21:33:12 -0500

    landing page

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return (
+      <nav className="bg-slate-950 p-4 fixed top-0 left-0 w-full z-10 border-b border-gray-300">
+        <ul className="flex space-x-14 justify-center">
+          {['About Me', 'Blog', 'Projects', 'Resume'].map((item) => (
+            <li key={item}>
+              <a
+                href={`#${item.toLowerCase().replace(' ', '-')}`}
+                className="text-white font-thin text-lg relative after:absolute after:w-full after:h-0.5 after:bg-cyan-300 after:left-0 after:bottom-0 after:origin-right after:scale-x-0 after:transition-transform after:duration-300 hover:after:origin-left hover:after:scale-x-100"
+              >
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
